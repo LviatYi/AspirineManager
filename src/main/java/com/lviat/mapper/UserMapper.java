114b3ce9f49a8d2b3f9d75574b667fbbffcca964
LviatYi <LviatYi@qq.com>
2021-08-21 21:11:03 +0800

    add UserMapper

---
+++ src/main/java/com/lviat/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package com.lviat.mapper;
+
+/**
+ * User 表 Mapper.
+ *
+ * @author LviatYi
+ * @version 1.0
+ * @interfaceName UserMapper
+ * @date 2021/8/21
+ */
+public interface UserMapper {
+}
