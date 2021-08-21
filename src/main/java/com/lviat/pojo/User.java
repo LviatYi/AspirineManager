366ed9f2fc58b3a6213ba1fec3818a457f5dfa39
LviatYi <LviatYi@qq.com>
2021-08-21 18:33:15 +0800

    add User entity

--- src/main/java/com/lviat/pojo/User.java
+++ src/main/java/com/lviat/pojo/User.java
@@ -1,12 +1,42 @@
 package com.lviat.pojo;
 
+import lombok.*;
+
 /**
- * TODO_LviatYi
+ * 用户 实体类.
  *
  * @author LviatYi
- * @version TODO_LviatYi
+ * @version 1.0
  * @className User
  * @date 2021/8/21
  */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    /**
+     * 用户 ID.
+     */
+    int userId;
+    /**
+     * 用户名.
+     */
+    String userName;
+    /**
+     * 密码
+     */
+    String password;
+    /**
+     * 用户全名
+     */
+    String userFullName;
+    /**
+     * 电话号码
+     */
+    String phoneNum;
+    /**
+     * 用户类型
+     */
+    int userType;
 }
---
+++ src/main/java/com/lviat/pojo/UserTypeEnum.java
@@ -0,0 +1,35 @@
+package com.lviat.pojo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 用户类型 Flag 枚举类.
+ *
+ * @author LviatYi
+ * @version 1.0
+ * @enumName UserTypeEnum
+ * @date 2021/8/21
+ */
+@Getter
+public enum UserTypeEnum {
+    /**
+     * 管理员.
+     */
+    ADMIN(0, "admin"),
+    VISITOR(1, "visitor");
+
+    /**
+     * 用户类型.
+     */
+    private final int userType;
+    /**
+     * 用户类型名称.
+     */
+    private final String typeName;
+
+    UserTypeEnum(int userType, String typeName) {
+        this.userType = userType;
+        this.typeName = typeName;
+    }
+}
