186190bea0df5ed44e73e977c99643c70715b4eb
LviatYi <LviatYi@qq.com>
2021-08-22 23:54:03 +0800

    神说：「要有 bug」.易之说：「要有 bug 杀手」. ————《圣经新语 | 乱糟福音》

---
+++ src/test/java/com/lviat/test/DoTest.java
@@ -0,0 +1,18 @@
+package com.lviat.test;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * TODO_LviatYi
+ *
+ * @author LviatYi
+ * @version TODO_LviatYi
+ * @className DoTest
+ * @date 2021/8/22
+ */
+public class DoTest {
+    @Test
+    public void test(){
+
+    }
+}
