7225f0984b214e0a5b5d6792daddcc2f53722b76
LviatYi <LviatYi@qq.com>
2021-08-22 23:49:53 +0800

    add MybatisUtil

---
+++ src/main/java/com/lviat/util/dao/MybatisUtil.java
@@ -0,0 +1,36 @@
+package com.lviat.util.dao;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Mybatis Util.
+ *
+ * @author LviatYi
+ * @version 1.0
+ * @className MybatisUtil
+ * @date 2021/8/22
+ */
+public class MybatisUtil {
+    public SqlSession getSession() {
+        SqlSession sqlSession = null;
+        try {
+            String resource = "mybatis-config.xml";
+            InputStream inputStream;
+            inputStream = Resources.getResourceAsStream(resource);
+            sqlSession = new SqlSessionFactoryBuilder().build(inputStream).openSession();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+
+        return sqlSession;
+    }
+
+    public void closeSession(SqlSession sqlSession) {
+        sqlSession.close();
+    }
+}
