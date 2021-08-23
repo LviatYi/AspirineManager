648050e2d78e82da592364519a55526c051b6b78
LviatYi <LviatYi@qq.com>
2021-08-23 12:23:00 +0800

    add Url constant text

---
+++ src/main/java/com/lviat/util/constant/text/UrlText.java
@@ -0,0 +1,24 @@
+package com.lviat.util.constant.text;
+
+/**
+ * URL 文本类.
+ *
+ * @author LviatYi
+ * @version 1.0
+ * @className UrlText
+ * @date 2021/8/23
+ */
+public class UrlText {
+    /**
+     * 登录页 Url.
+     */
+    final public static String URL_LOGIN = "/login";
+    /**
+     * 登出页 Url.
+     */
+    final public static String URL_LOGOUT = "/logout";
+    /**
+     * 用户管理页 Url.
+     */
+    final public static String URL_USER_MANAGER = "/admin/user-manage";
+}
