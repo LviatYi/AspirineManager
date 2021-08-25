44092303df093febf86724c5595061c26a835224
LviatYi <LviatYi@qq.com>
2021-08-25 13:25:48 +0800

    update RelationText

--- src/main/java/com/lviat/util/constant/text/RelationText.java
+++ src/main/java/com/lviat/util/constant/text/RelationText.java
@@ -15,7 +15,10 @@ public class RelationText {
     final public static String SALES_INFO_TABLE_NAME = "SalesInfo";
     final public static String USER_TABLE_NAME = "User";
 
-    final public static String Web_USER_ID = "user_id";
-    final public static String Web_USER_NAME = "user_name";
-    final public static String Web_USER_PASSWORD = "user_password";
+    final public static String WEB_USER_ID = "user_id";
+    final public static String WEB_USER_USERNAME = "user_username";
+    final public static String WEB_USER_NAME = "user_name";
+    final public static String WEB_USER_PASSWORD = "user_password";
+    final public static String WEB_USER_PHONE_NUM = "user_phone_num";
+    final public static String WEB_USER_ROLE = "user_role";
 }
