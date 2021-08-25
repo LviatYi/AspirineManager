ed3c733db25a0d545e9145c5cf11483b6665bbdb
LviatYi <LviatYi@qq.com>
2021-08-25 13:26:02 +0800

    add UserLoginServlet

--- src/main/java/com/lviat/servlet/UserLoginServlet.java
+++ src/main/java/com/lviat/servlet/UserLoginServlet.java
@@ -1,71 +1,91 @@
-//package com.lviat.servlet;
-//
-//import com.lviat.pojo.User;
-//import com.lviat.service.user.UserServiceImpl;
-//import com.lviat.tool.Token;
-//import com.lviat.tool.constant.text.zh_cn.*;
-//import jakarta.servlet.ServletException;
-//import jakarta.servlet.annotation.WebServlet;
-//import jakarta.servlet.http.HttpServlet;
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
-//import jakarta.servlet.http.HttpSession;
-//import lombok.extern.slf4j.Slf4j;
-//
-//import java.io.IOException;
-//
-///**
-// * 用户登录后台.
-// * 无权限.
-// *
-// * @author LviatYi
-// * @version 1.0
-// * @className UserLoginServlet
-// * @date 2021/7/28
-// */
-//@Slf4j
-//@WebServlet(name = "UserLoginServlet", urlPatterns = UrlText.URL_LOGIN + ".do")
-//public class UserLoginServlet extends HttpServlet {
-//    UserServiceImpl userService;
-//
-//    public UserLoginServlet() {
-//        userService = new UserServiceImpl();
-//    }
-//
-//    @Override
-//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        doPost(req, resp);
-//    }
-//
-//    @Override
-//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        if (req.getParameter(UserText.USER_ID) != null && req.getParameter(UserText.USER_PASSWORD) != null) {
-//            int userId = Integer.parseInt(req.getParameter(UserText.USER_ID));
-//            String password = req.getParameter(UserText.USER_PASSWORD);
-//
-//            if (userService.verifyPassword(userId, password)) {
-//                userService.updateLastLoginTime(userId);
-//
-//                User loginUser = userService.getUser(userId);
-//                resp.setHeader("token", Token.createToken(userId, loginUser.getAuthentic()));
-//
-//                HttpSession httpSession = req.getSession();
-//                httpSession.setAttribute(UserText.USER_ID, String.valueOf(loginUser.getId()));
-//                httpSession.setAttribute(UserText.USER_NAME, String.valueOf(loginUser.getUserName()));
-//                httpSession.setAttribute(UserText.USER_LAST_LOGIN_TIME, String.valueOf(loginUser.getLastLoginTime()));
-//                httpSession.setAttribute(UserText.USER_CREATE_TIME, String.valueOf(loginUser.getCreateTime()));
-//
-//                log.info("User login.Id : " + userId);
-//                resp.sendRedirect("/index.jsp");
-//                return;
-//            }
-//        }
-//
-//        /*
-//        * TODO_LviatYi 建议只返回状态，不重定向.
-//        * date 2021/8/1
-//        */
-//        req.setAttribute("error", MessageText.PASSWORD_ERROR);
-//        resp.sendRedirect("/login.jsp");
-//    }
-//}
+package com.lviat.servlet;
+
+import com.lviat.model.User;
+import com.lviat.service.UserServiceImpl;
+import com.lviat.service.UserServiceStatus;
+import com.lviat.util.authentic.TokenUtil;
+import com.lviat.util.constant.text.RelationText;
+import com.lviat.util.constant.text.UrlText;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+
+/**
+ * 用户登录后台.
+ * 无权限.
+ *
+ * @author LviatYi
+ * @version 1.0
+ * @className UserLoginServlet
+ * @date 2021/7/28
+ */
+@Slf4j
+@WebServlet(name = "UserLoginServlet", urlPatterns = UrlText.URL_LOGIN + ".do")
+public class UserLoginServlet extends HttpServlet {
+    UserServiceImpl userService;
+
+    public UserLoginServlet() {
+        userService = new UserServiceImpl();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        UserServiceStatus status = UserServiceStatus.SUCCESSFUL;
+
+        if (req.getParameter(RelationText.WEB_USER_ID) == null || req.getParameter(RelationText.WEB_USER_PASSWORD) == null) {
+            status = UserServiceStatus.VERITY_NOT_PASS;
+        } else {
+            long userId = Long.parseLong(req.getParameter(RelationText.WEB_USER_ID));
+            String password = req.getParameter(RelationText.WEB_USER_PASSWORD);
+
+            status = userService.verifyPassword(userId, password);
+
+            User loginUser = new User();
+            if (status == UserServiceStatus.SUCCESSFUL) {
+                status = userService.getUser(loginUser, userId);
+            }
+
+            if (status == UserServiceStatus.SUCCESSFUL) {
+                resp.setHeader("token", TokenUtil.createToken(userId, loginUser.getRole()));
+
+                HttpSession httpSession = req.getSession();
+
+                httpSession.setAttribute(RelationText.WEB_USER_ID, loginUser.getId());
+                httpSession.setAttribute(RelationText.WEB_USER_USERNAME, loginUser.getUsername());
+                httpSession.setAttribute(RelationText.WEB_USER_NAME, loginUser.getUsername());
+                httpSession.setAttribute(RelationText.WEB_USER_PHONE_NUM, loginUser.getPhoneNum());
+                httpSession.setAttribute(RelationText.WEB_USER_ROLE, loginUser.getRole());
+            }
+
+            switch (status) {
+                case UNKNOWN_ERROR:
+                default:
+                    /*
+                     * TODO_LviatYi 提示前端错误信息.
+                     * date 2021/8/25
+                     */
+                    break;
+                case SUCCESSFUL:
+                    log.info("User login.Id : " + userId);
+                    resp.sendRedirect(UrlText.URL_LOGIN + ".jsp");
+                case VERITY_NOT_PASS:
+                    /*
+                     * TODO_LviatYi 提示前端密码错误.
+                     * date 2021/8/25
+                     */
+                    break;
+            }
+        }
+    }
+}
