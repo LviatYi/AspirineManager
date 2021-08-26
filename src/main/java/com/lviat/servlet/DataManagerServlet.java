1d8a1c0323ea0ce88cf9094a2e3f06b0adbef443
nameLair-design <2486420157@qq.com>
2021-08-26 17:47:30 +0800

    add DataManagerServlet

--- src/main/java/com/lviat/servlet/DataManagerServlet.java
+++ src/main/java/com/lviat/servlet/DataManagerServlet.java
@@ -1,10 +1,8 @@
 package com.lviat.servlet;
 
 import com.alibaba.fastjson.JSON;
-import com.lviat.model.User;
-import com.lviat.service.UserService;
-import com.lviat.service.UserServiceImpl;
-import com.lviat.service.UserServiceStatus;
+import com.lviat.model.*;
+import com.lviat.service.*;
 import com.lviat.util.authentic.TokenUtil;
 import com.lviat.util.constant.text.RelationText;
 import com.lviat.util.constant.text.UrlText;
@@ -27,6 +25,10 @@ import java.io.IOException;
  */
 public class DataManagerServlet extends HttpServlet {
     private UserService userService;
+    private MedicineService medicineService;
+    private SalesInfoService salesInfoService;
+    private MedicineTypeService medicineTypeService;
+    private ConsumerService consumerService;
 
     public DataManagerServlet() {
         this.userService = new UserServiceImpl();
@@ -39,10 +41,39 @@ public class DataManagerServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        User newUser = JSON.parseObject(req.getParameter("user"), User.class);
+        Medicine newMedicine  = JSON.parseObject(req.getParameter("medicine"),Medicine.class);
+        MedicineType newMedicineType = JSON.parseObject(req.getParameter("medicineType"),MedicineType.class);
         switch (req.getParameter("method")) {
             case "user-add":
-                User newUser = JSON.parseObject(req.getParameter("user"), User.class);
                 userService.addUser(newUser);
+                break;
+            case "user-del":
+                userService.delUser(newUser.getId());
+                break;
+            case "user-modify":
+                userService.modifyUser(newUser);
+                break;
+
+            case "medicine-add":
+                medicineService.addMedicine(newMedicine);
+                break;
+            case "medicine-del":
+                medicineService.delMedicine(newMedicine.getId());
+                break;
+            case "medicine-modify":
+                medicineService.modifyMedicine(newMedicine);
+                break;
+
+            case "medicineType-add":
+                medicineTypeService.addMedicineType(newMedicineType);
+                break;
+            case "medicineType-del":
+                medicineTypeService.delMedicineType(newMedicineType.getTypeId());
+                break;
+            case "medicineType-modify":
+                medicineTypeService.modifyMedicineType(newMedicineType);
+                break;
             default:
                 break;
         }
