f6ff3b316de0bc0aeaf069254cdd8baa6cf9ec36
LviatYi <LviatYi@qq.com>
2021-08-26 13:49:26 +0800

    add MedicineService

---
+++ src/main/java/com/lviat/service/MedicineServiceImpl.java
@@ -0,0 +1,12 @@
+package com.lviat.service;
+
+/**
+ * 药品 Service impl.
+ *
+ * @author LviatYi
+ * @version 1.0
+ * @className MedicineServiceImpl
+ * @date 2021/8/26
+ */
+public class MedicineServiceImpl implements MedicineService {
+}
