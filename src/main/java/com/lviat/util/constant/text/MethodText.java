4b0dde70ec3dd1c96e2496b649744f4be5947899
LviatYi <LviatYi@qq.com>
2021-08-26 23:50:00 +0800

    add MethodText

---
+++ src/main/java/com/lviat/util/constant/text/MethodText.java
@@ -0,0 +1,29 @@
+package com.lviat.util.constant.text;
+
+/**
+ * Web 方法 文本类.
+ *
+ * @author LviatYi
+ * @version 1.0
+ * @className MethodText
+ * @date 2021/8/26
+ */
+public class MethodText {
+    final public static String METHOD = "method";
+
+    final public static String USER_ADD = "user-add";
+    final public static String USER_DEL = "user-del";
+    final public static String USER_MODIFY = "user-modify";
+    final public static String MEDICINE_ADD = "medicine-add";
+    final public static String MEDICINE_DEL = "medicine-del";
+    final public static String MEDICINE_MODIFY = "medicine-modify";
+    final public static String MEDICINE_TYPE_ADD = "medicineType-add";
+    final public static String MEDICINE_TYPE_DEL = "medicineType-del";
+    final public static String MEDICINE_TYPE_MODIFY = "medicineType-modify";
+    final public static String CONSUMER_ADD = "consumer-add";
+    final public static String CONSUMER_DEL = "consumer-del";
+    final public static String CONSUMER_MODIFY = "consumer-modify";
+    final public static String SALES_INFO_ADD = "salesInfo-add";
+    final public static String SALES_INFO_DEL = "salesInfo-del";
+    final public static String SALES_INFO_MODIFY = "salesInfo-modify";
+}
