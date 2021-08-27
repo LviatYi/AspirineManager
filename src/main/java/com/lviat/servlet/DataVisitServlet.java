790ccd3ee93d5fb7ee8880f333394a0e4be7fef2
Titc <1240104681@qq.com>
2021-08-27 10:57:51 +0800

    qwq

--- src/main/java/com/lviat/servlet/DataVisitServlet.java
+++ src/main/java/com/lviat/servlet/DataVisitServlet.java
@@ -1,30 +1,23 @@
 package com.lviat.servlet;
 
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
-import com.github.pagehelper.IPage;
 import com.lviat.model.Medicine;
-import com.alibaba.fastjson.JSON;
 import com.lviat.model.*;
 import com.lviat.service.*;
-import com.lviat.util.authentic.TokenUtil;
 import com.lviat.util.constant.text.MethodText;
 import com.lviat.util.constant.text.RelationText;
-import com.lviat.util.constant.text.UrlText;
 
 
-import com.sun.deploy.net.HttpResponse;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 
-import java.lang.reflect.Method;
+
+
 import java.util.ArrayList;
 import java.util.List;
 
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
+
 
 
 import java.io.IOException;
@@ -57,30 +50,30 @@ public class DataVisitServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("utf-8");
         switch (req.getParameter(MethodText.METHOD)) {
-            case MethodText.USER_GET:
+            case MethodText.USER_SELECT:
                 this.getUser(req, resp);
                 break;
-            case MethodText.USER_GET_LIST:
+            case MethodText.USER_SELECT_LIST:
                 this.getUserList(req,resp);
                 break;
-            case MethodText.MEDICINE_GET:
+            case MethodText.MEDICINE_SELECT:
                 this.getMedicine(req, resp);
                 break;
-            case MethodText.MEDICINE_GET_LIST:
+            case MethodText.MEDICINE_SELECT_LIST:
                 this.getMedicineList(req,resp);
                 break;
-            case MethodText.MEDICINE_TYPE_GET_LIST:
+            case MethodText.MEDICINE_TYPE_SELECT_LIST:
                 this.getMedicineTypeList(req, resp);
                 break;
-            case MethodText.CONSUMER_GET:
+            case MethodText.CONSUMER_SELECT:
                 this.getConsumer(req, resp);
                 break;
-            case MethodText.CONSUMER_GET_LIST:
+            case MethodText.CONSUMER_SELECT_LIST:
                 this.getConsumerList(req,resp);
                 break;
-            case MethodText.SALES_INFO_GET:
+            case MethodText.SALES_INFO_SELECT:
                 this.getSalesInfo(req, resp);
-            case MethodText.SALES_INFO_GET_LIST:
+            case MethodText.SALES_INFO_SELECT_LIST:
                 this.getSalesInfoList(req,resp);
                 break;
             default:
